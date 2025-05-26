76f29eb88538d2eb06d648855ef2f834261d84b7
Bill Cipher <billcipher@DESKTOP-FTJI5I7>
2025-05-26 17:44:43 +0200

    Add header

--- srcs/core/minishell.c
+++ srcs/core/minishell.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: billcipher <billcipher@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 17:44:25 by billcipher        #+#    #+#             */
+/*   Updated: 2025/05/26 17:44:26 by billcipher       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -10,9 +22,12 @@ int	main(void)
 	while (1)
 	{
 		line = readline("minishell> ");
-		if (is_open_quotes(line, '"') || is_open_quotes(line, '\''))
-			printf("Open quote\n");
 		add_history(line);
+		if (is_open_quotes(line, '"') || is_open_quotes(line, '\''))
+		{
+			printf("Open quote\n");
+			continue;
+		}
 	}
 	rl_clear_history();
 	return (0);
