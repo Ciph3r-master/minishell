c3e067e4c2e084166e34654b1018e721b34c2415
Thibaud Maitre <thmaitre@z2r3p5.42lyon.fr>
2025-06-11 20:38:10 +0200

    🔧 : remove printf

--- srcs/data/init_data.c
+++ srcs/data/init_data.c
@@ -14,16 +14,11 @@
 #include "libft.h"
 #include "minishell.h"
 
-	#include <stdio.h>
-
-//faire la gestion des erreur
 int	init_data(t_data *data, char **env)
 {
 	data->env_list = get_env_list(data, env);
-	print_env_list(&data->env_list);
 	data->env_copy = get_env_copy(data->env_list);
 	if (!data->env_copy)
 		free_and_exit(data);
-	print_env_copy(data->env_copy);
 	return (0);
 }
