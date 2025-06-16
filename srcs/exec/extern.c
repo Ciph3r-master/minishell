56aa6e0e7377549d753131382bfead37274837d2
Thibaud Maitre <thibaud@pc-24.home>
2025-06-16 19:42:28 +0200

    🚧 WIP: exec_extern

--- srcs/exec/extern.c
+++ srcs/exec/extern.c
@@ -6,19 +6,83 @@
 /*   By: thibaud <thibaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 23:47:00 by thibaud           #+#    #+#             */
-/*   Updated: 2025/06/16 19:01:39 by thibaud          ###   ########.fr       */
+/*   Updated: 2025/06/16 19:41:22 by thibaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include "minishell.h"
+#include "libft.h"
 
-int exec_extern(t_cmd_node *cmd_node)
+int	find_path_with_access(char **paths, char **pathname)
 {
+	int	i;
 
+	i = 0;
+	while (paths[i])
+	{
+		if (0 == access(paths[i], X_OK))
+		{
+			*pathname = ft_strdup(paths[i]);
+			if (NULL == *pathname)
+			{
+				free_char_tab_all(paths);
+				return (-1);
+			}
+			return (1);
+		}
+		i++;
+	}
+	return (-2);
+}
+
+int	get_cmd_path_name(char **pathname, t_cmd_node *cmd_node)
+{
+	char	*path;
+	char	**paths;
+
+	path = getenv("PATH");
+	if (NULL == path)
+		return (-1);
+	paths = ft_split_set(path, ":");
+	if (ft_add_string_to_strings(paths, "/") == NULL)
+		return (-1);
+	if (ft_add_string_to_strings(paths, cmd_node->cmd->args[0]) == NULL)
+		return (-1);
+	if (find_path_with_access(paths, pathname) != 1)
+	{
+		ft_free_char_tab_all(paths);
+		return (-1);
+	}
+	ft_free_char_tab_all(paths);
 	return (1);
 }
 
+int exec_extern(t_cmd_node *cmd_node)
+{
+	char	*pathname;
+	int		exec_out;
+
+	pathname = NULL;
+	exec_out = 1;
+	if (!cmd_node || !cmd_node->cmd->cmd)
+		return (-1);
+	exec_out = get_cmd_path_name(&pathname, cmd_node);
+	if (exec_out == -2)
+		printf("minishell: %s: command not found", cmd_node->cmd->cmd);
+	return (exec_out);
+}
+
+//on doit checker avant toute chose si la commande est un directory
+// 1 la commande a ete execute avec succes
+// -1 free and exit
+// -2 la commande n'existe pas, est un directory, un file_in n'existe pas
+
+//on a fait les redirection avec retour d'erreur -1 et -2
+//on "sait" si ce n'est pas un directory avec erreur -1 et -2
+//on doit maintenant trouver et executer la commande avec erreur -2
+//	si n'existe pas et erreur -1 en cas de pb
 int exec_simple_cmd_extern(t_cmd_node *cmd_node)
 {
 	int	exec_out;
@@ -32,8 +96,11 @@ int exec_simple_cmd_extern(t_cmd_node *cmd_node)
 	exec_out = exec_redirections(cmd_node);
 	if (exec_out != 1)
 		return (exec_out);
-	if (exec_extern(cmd_node) != 1)
-		return (-1);
+	// if (cmd_is_directory(cmd_node))
+	// 	return (-2);
+	exec_out = exec_extern(cmd_node);
+	if (exec_out != 1)
+		return (exec_out);
 	if (reset_stdin_stdout(saved_stdin, saved_stdout) != 1)
 		return (-1);
 	return (1);
