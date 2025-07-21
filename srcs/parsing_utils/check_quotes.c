1dda4622ecc3d910a6a4295985d180d1ce969b08
Quentin Truchet <quentintruchet24@gmail.com>
2025-07-22 00:18:55 +0200

    Check quotes

--- srcs/parsing_utils/check_quotes.c
+++ srcs/parsing_utils/check_quotes.c
@@ -3,33 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   check_quotes.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vscode <vscode@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qutruche <qutruche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 23:35:08 by vscode            #+#    #+#             */
-/*   Updated: 2025/07/18 23:36:34 by vscode           ###   ########.fr       */
+/*   Updated: 2025/07/22 00:16:19 by qutruche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "unistd.h"
 
 bool	check_quotes(char *line)
 {
-	int	i;
-	int	dquotes;
-	int	quotes;
+	int		i;
+	char	current_quote;
 
-	dquotes = 0;
-	quotes = 0;
 	i = 0;
+	current_quote = 0;
 	while (line[i])
 	{
-		if (line[i] == '"')
-			dquotes++;
-		if (line[i] == '\'')
-			quotes++;
+		if (current_quote == 0 && (line[i] == '"' || line[i] == '\''))
+			current_quote = line[i];
+		else if (line[i] == current_quote)
+			current_quote = 0;
 		i++;
 	}
-	if (dquotes % 2 != 0 || quotes % 2 != 0)
+	if (current_quote != 0)
+	{
+		write(STDERR_FILENO, "minishell: error: unclosed quotes\n", 35);
 		return (false);
+	}
 	return (true);
 }
